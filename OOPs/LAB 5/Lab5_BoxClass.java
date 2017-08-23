41cc480a9e6189d84ecce00daf6710e6cb11f6b5
Pranav Tharoor <pranav.tharoor@gmail.com>
2017-08-23 17:10:00 +0530

    OOP - Lab 5

---
+++ OOPs/LAB 5/Lab5_BoxClass.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab;
+
+import java.util.Scanner;
+
+/**
+ * Calculates volume of a box
+ * @author Pranav Tharoor
+ */
+
+class Box {
+    double length, width, height;
+    public void setDimentions(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+    public double volume() {
+        return length * width * height; 
+    }
+}
+
+public class Lab5_BoxClass {
+    public static void main(String args[]) {
+        Box b = new Box();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter dimentions of box");
+        b.setDimentions(in.nextDouble(), in.nextDouble(), in.nextDouble());
+        System.out.println("Volume = " + b.volume());
+    }
+}
---
+++ OOPs/LAB 5/Lab5_TimeClass.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab;
+
+import java.util.Scanner;
+
+/**
+ * Sum of time
+ * @author Pranav Tharoor
+ */
+
+class Time {
+    int hours, mins, secs;
+    Time() {
+        hours = 0;
+        mins = 0;
+        secs = 0;
+    }
+    Time(int hours, int mins, int secs) {
+        this.hours = hours;
+        this.mins = mins;
+        this.secs = secs;
+    }
+    public void display() {
+        System.out.print(hours + ":" + mins + ":" + secs);
+    }
+    public Time addTimes(Time t1, Time t2) {
+        Time t = new Time();
+        t.secs = (t1.secs + t2.secs) % 60;
+        int temp = (t1.secs + t2.secs) / 60;
+        t.mins = (t1.mins + t2.mins + temp) % 60;
+        temp = (t1.mins + t2.mins + temp) / 60;
+        t.hours = t1.hours + t2.hours + temp;
+        return t;
+    }
+}
+
+public class Lab5_TimeClass {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter hours, mins and seconds of time 1");
+        Time t1 = new Time(in.nextInt(), in.nextInt(), in.nextInt());
+        System.out.println("Enter hours, mins and seconds of time 2");
+        Time t2 = new Time(in.nextInt(), in.nextInt(), in.nextInt());
+        System.out.println("Sum of times: ");
+        t1.display();
+        System.out.print(" + ");
+        t2.display();
+        System.out.print(" = ");
+        Time t3 = t1.addTimes(t1, t2);
+        t3.display();
+    }   
+}
