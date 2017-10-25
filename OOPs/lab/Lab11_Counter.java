f018f5ee814c7f2b272bddc92eda726b383fb7ff
pranavtharoor <pranav.tharoor@gmail.com>
2017-10-25 12:13:49 +0530

    Lab 11 OOPs

--- OOPs/lab/Lab11_Counter.java
+++ OOPs/lab/Lab11_Counter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab;
+// package lab;
 
 /**
  * Counter
@@ -15,21 +15,28 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-class CounterGUI {
+class CounterGUI extends JFrame {
     
     JLabel count = new JLabel("0");
     JButton incrementButton = new JButton("+6");
     ListenForButton buttonClicked = new ListenForButton();
-    
+    JPanel mainPanel = new JPanel();
+
     public CounterGUI() {
+        mainPanel.add(count);
+        mainPanel.add(incrementButton);
+        this.add(mainPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         incrementButton.addActionListener(buttonClicked);
-        
+        this.setSize(100, 80);
+        this.setVisible(true);
     }
     
     class ListenForButton implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            
+            if(e.getSource() == incrementButton)
+                count.setText((Integer.parseInt(count.getText()) + 6) + "");
         }
     }
     
